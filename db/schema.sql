8993851636f7fee38a89ef74f8642ab02f8e0558
Brian Alegre <brialegre@yahoo.com>
2022-06-16 16:32:44 -0700

    Updated Schema and Seeds

--- db/schema.sql
+++ db/schema.sql
@@ -11,28 +11,33 @@ USE employees_db;
 
 -- Create Departments Table
 CREATE TABLE departments (
-    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) UNIQUE NOT NULL
 );
 
 -- Create Roles Table
 CREATE TABLE roles (
-    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    role_title VARCHAR(30) NOT NULL,
-    role_salary INT NOT NULL,
-    department_id VARCHAR(30) NOT NULL
-    -- Add FORIEGN and REFERENCE to Departments Table
-    
+  role_id INT AUTO_INCREMENT PRIMARY KEY,
+  role_title VARCHAR(30) UNIQUE NOT NULL,
+  role_salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(department_id) ON DELETE CASCADE
 );
 
 -- Create Employees Table
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL, 
-    -- Add FOREIGN AND REFERNCE to Roles Table
-    manager_id INT
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE,
+  manager_id INT,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE
+  SET
+    NULL
 );
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,40 +4,32 @@ USE employees_db;
 
 -- Insert TD to Departments Table
 INSERT INTO departments (department_name)
-VALUES 
-    ("Engineering"), 
-    ("Human Resources"),
-    ("Sales"),
-    ("Facilities");
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
 -- Insert TD to Roles Table
 INSERT INTO roles (role_title, role_salary, department_id)
-VALUES 
-    ("Development Manager", 200000, 1),
-    ("Software Engineer", 100000, 1),
-    ("Testing Technician", 80000, 1), 
-    ("HR Manager", 180000, 2),
-    ("Benefits", 80000, 2),
-    ("Generals", 75000, 2),
-    ("Marketing Manager", 110000, 3),
-    ("Marketing", 220000, 3),
-    ("Salesperson", 200000, 3),
-    ("Facilities Manager", 150000, 3),
-    ("Security", 60000, 4), 
-    ("Sanitation Engineer", 62000, 4);
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
 -- Insert TD to Employees Table
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES 
-    ("Brian", "Alegre", 1, null),
-    ("Jayson", "De Guzman", 2, null),
-    ("Krizel", "Adupe", 3, null),
-    ("Kalie", "Brwon", 4, null),
-    ("Chad", "Tao", 5, null),
-    ("Nick", "Graffis", 6, null),
-    ("Allec", "Arzadon", 7, null),
-    ("Kevin",  "Lazaro", 8, null),
-    ("Tony",  "Wu", 9, null),
-    ("Matthias",  "Aretz", 10, null),
-    ("Long",  "Nguyen", 11, null),
-    ("Mark",  "Baker", 12, null);
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
