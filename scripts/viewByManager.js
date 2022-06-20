f9c11005235d313bf2cb6db0750d142e6317d59c
Brian Alegre <brialegre@yahoo.com>
2022-06-19 22:31:46 -0700

    Added fucntionality to view employees by manager

--- scripts/viewByManager.js
+++ scripts/viewByManager.js
@@ -4,28 +4,53 @@
 const db = require('../config/connection');
 const inquirer = require('inquirer');
 
-// View Employees By Manager
+
+// Get Manager IDs
 async function viewByManager() {
-    // List Possible Managers
-    const [manager] = await db.promise().query("SELECT manager_id, first_name, last_name FROM employees")
-    const managerChoices = manager.map(man => {
-        return {
-            name: `${man.first_name} ${man.last_name}`,
-            value: man.manager_id
-        }
-    })
+    // const allManagers = await db.promise().query('SELECT manager_id FROM employees WHERE manager_id IS NOT NULL');
+    const allManagers = await db.promise().query('SELECT first_name, last_name, id FROM employees WHERE (id IN (SELECT manager_id FROM employees));');
+
+    const managerIDs = allManagers[0].map((m) => ({
+        name: `${m.first_name} ${m.last_name}`,
+        value: m.id,
+    }));
+
     const { manager_id } = await inquirer.prompt([
         {
-            name: "manager_id",
-            message: "Select Manager to View their Employees:",
-            type: "list",
-            choices: managerChoices,
-        },
-    ])
-    // List Employees by Manager
-    const viewManEmployees = await db.promise().query(`SELECT first_name, last_name FROM employees WHERE manager_id = ?`, [manager_id])
+            type: 'list',
+            name: 'manager_id',
+            message: 'Select Manager:',
+            choices: managerIDs,
+
+        }
+    ]);
+    const viewManEmployees = await db.promise().query(`SELECT * FROM employees WHERE manager_id = ${manager_id}`);
     return viewManEmployees;
 }
+
+
+// View Employees By Manager
+// async function viewByManager() {
+//     // List Managers
+//     const [manager] = await db.promise().query(`SELECT first_name, last_name, manager_id FROM employees WHERE manager_id IS NOT NULL`);
+//     const managerChoices = manager.map((man) => {
+//         return {
+//             name: `${man.first_name} ${man.last_name}`,
+//             value: man.manager_id
+//         }
+//     })
+//     const { manager_id } = await inquirer.prompt([
+//         {
+//             name: "manager_id",
+//             message: "Select Manager to View their Employees:",
+//             type: "list",
+//             choices: managerChoices,
+//         },
+//     ])
+//     // List Employees by Manager
+//     const viewManEmployees = await db.promise().query(`SELECT first_name, last_name FROM employees WHERE manager_id = ?`, [manager_id])
+//     return viewManEmployees;
+// }
 
 // Export
 module.exports = viewByManager;
